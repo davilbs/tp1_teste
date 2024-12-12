6dc6240bcfa983e32bc3d483e065bb4b8e4a0699
davilage.borges@gmail.com <davilage.borges@gmail.com>
2024-12-12 01:21:36 -0300

    Update base tests

--- test_estoque.py
+++ test_estoque.py
@@ -1,9 +1,14 @@
 import unittest
 
-from estoque import EstoqueAlimento
-
+from estoque import Estoque
+from produto import Produto
 class TestEstoque(unittest.TestCase):
+    def test_buy_one_product(self):
+        estoque = Estoque()
+        estoque.buy_produto('Vassoura', 10)
+        self.assertEqual(estoque.get_produto_amount('Vassoura'), 10)
+
     def test_add_one_product(self):
-        estoque = EstoqueAlimento()
-        estoque.buy_alimento('arroz', 10)
-        self.assertEqual(estoque.get_alimento('arroz'), 10)
+        estoque = Estoque()
+        estoque.add_produto(Produto('Vassoura', 10))
+        self.assertEqual(str(estoque.get_produto_info('Vassoura')), "Vassoura - R$ 10.00")
