c06f3e7757fec943c6a8f28f40c5853dfdcc43a2
davilbs <davilage.borges@gmail.com>
2024-12-12 19:39:25 -0300

    Update Produto parameters types

--- produto.py
+++ produto.py
@@ -1,10 +1,16 @@
 class Produto:
-    def __init__(self, nome, preco, marca=None):
+    nome: str
+    preco: float
+    marca: set
+    
+    def __init__(self, nome: str, preco: float, marca: str = None):
+        if preco <= 0:
+            raise ValueError('Preço deve ser maior que zero')
         self.nome = nome
         self.preco = preco
-        self.marcas = []
+        self.marcas = set()
         if marca:
-            self.marcas.append(marca)
+            self.marcas.add(marca)
 
     def __str__(self):
         return f'{self.nome} - R$ {self.preco:.2f}'
@@ -13,13 +19,13 @@ class Produto:
         return f'Produto({self.nome}, {self.preco})'
     
     def add_marca(self, marca):
-        self.marcas.append(marca)
+        self.marcas.add(marca)
 
     def remove_marca(self, marca):
         self.marcas.remove(marca)
 
 class ProdutoAlimento(Produto):
-    def __init__(self, nome, preco, validade=None, vegano=False, marca=None):
+    def __init__(self, nome, preco, validade=None, vegano: bool = False, marca=None):
         super().__init__(nome, preco, marca)
         self.vegano = vegano
         self.validade = validade
