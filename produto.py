b729781f91905157b936ed51b8a350511119b00a
davilbs <davilage.borges@gmail.com>
2024-12-12 20:07:18 -0300

    Add conversão para datetime da validade

--- produto.py
+++ produto.py
@@ -1,8 +1,9 @@
+import datetime
 class Produto:
     nome: str
     preco: float
     marca: set
-    
+
     def __init__(self, nome: str, preco: float, marca: str = None):
         if preco <= 0:
             raise ValueError('Preço deve ser maior que zero')
@@ -25,13 +26,17 @@ class Produto:
         self.marcas.remove(marca)
 
 class ProdutoAlimento(Produto):
-    def __init__(self, nome, preco, validade=None, vegano: bool = False, marca=None):
+    validade: datetime.date
+    def __init__(self, nome, preco, validade: str = None, vegano: bool = False, marca=None):
         super().__init__(nome, preco, marca)
         self.vegano = vegano
-        self.validade = validade
+        if validade:
+            self.set_validade(validade)
+        else:
+            self.validade = None
 
     def __str__(self):
-        return f'{self.nome} - R$ {self.preco:.2f} {"vegano" if self.vegano else ""} {f"- {self.validade}" if self.validade else ""}'
+        return f'{self.nome} - R$ {self.preco:.2f}{" vegano" if self.vegano else ""}{f" - {self.validade.isoformat()}" if self.validade else ""}'
 
     def __repr__(self):
         return f'ProdutoAlimento({self.nome}, {self.preco}, {self.vegano}, {self.validade})'
@@ -39,5 +44,5 @@ class ProdutoAlimento(Produto):
     def set_vegano(self, vegano):
         self.vegano = vegano
     
-    def set_validade(self, validade):
-        self.validade = validade
+    def set_validade(self, validade: str):
+        self.validade = datetime.datetime.strptime(validade, '%Y-%m-%d').date()
