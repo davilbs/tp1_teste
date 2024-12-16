1b0d4b48b08960ac976a6dba94ba710cd0869e9f
davilage.borges@gmail.com <davilage.borges@gmail.com>
2024-12-15 21:37:18 -0300

    Add funções para desconto e alterar nome

--- business.py
+++ business.py
@@ -7,20 +7,38 @@ class Business():
         self._estoque = estoque
         self._transactions = []
         self._margin = margin
+        self.historico = []
+        self.desconto = 0
 
-    def calculate_profit(self):
+    def calculate_profit(self) -> float:
         profit = 0
         for transaction in self._transactions:
             profit += transaction[0] * transaction[1]
+        self.registrar_historico(f'Lucro total: R$ {profit:.2f}')
         return profit
     
-    def get_estoque_size(self):
+    def registrar_historico(self, mensagem: str):
+        self.historico.append(mensagem)
+
+    def alterar_nome(self, novo_nome: str):
+        if not novo_nome:
+            raise ValueError('O nome não pode ser vazio')
+        self.nome = novo_nome
+        self.registrar_historico(f'Nome alterado para "{novo_nome}"')
+
+    def set_desconto(self, percentual: float):
+        if percentual < 0 or percentual > 100:
+            raise ValueError('O desconto deve estar entre 0% e 100%')
+        self.desconto = percentual
+        self.registrar_historico(f'Promoção de {percentual}%')
+
+    def get_estoque_size(self) -> int:
         return self._estoque.get_estoque_size()
     
-    def get_produto_info(self, nome):
+    def get_produto_info(self, nome) -> Produto | None:
         return self._estoque.get_produto_info(nome)
     
-    def calculate_buy_price(self, preco):
+    def calculate_buy_price(self, preco) -> float:
         # O preço de compra é o preço de venda + a margem
         return -(preco * (1 - self._margin))
     
@@ -30,10 +48,14 @@ class Business():
             return False
         
         if ttype == 'sell':
+            produto.aplicar_desconto(self.desconto)
             self._transactions.append((produto.preco, amount))
+            produto.restaurar_preco(produto.preco)
+            self.registrar_historico(f'Venda de {amount} unidades de {nome}')
         else:
             buy_price = self.calculate_buy_price(produto.preco)
             self._transactions.append((buy_price, amount))
+            self.registrar_historico(f'Compra de {amount} unidades de {nome}')
 
         return True
 
